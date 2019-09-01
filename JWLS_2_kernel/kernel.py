0e529100cbac79f5775f527fafc19b59b2df9a5b
Nicola Cattabiani <fortsaint@gmail.com>
2019-09-01 15:06:22 +0200

    stable

--- JWLS_2_kernel/kernel.py
+++ JWLS_2_kernel/kernel.py
@@ -2,12 +2,12 @@ from ipykernel.kernelbase import Kernel
 import os
 
 
-
-# W completions
 WNames = open('/home/nicola/Gits/JWLS_2/JWLS_2_kernel/Names.wl.txt',
               'r').read().split()
 
+######################################################################################
 class JWLS_2_kernel(Kernel):
+    
     implementation = 'JWLS_2'
     implementation_version = '2.0'
     language = 'Wolfram Language'
@@ -19,17 +19,14 @@ class JWLS_2_kernel(Kernel):
     banner = "Jupyter for Wolfram Language Scripts"
 
     
-    
-    
+    ##################################################################################
     def do_execute(self, code, silent, store_history=False, user_expressions=None,
                    allow_stdin=False):
         
-        if not silent:
-            
-            wl_response = os.popen('echo '+"'"+code+"'"+' | nc 127.0.0.1 5858').read()
+        wl_response = os.popen('echo '+"'"+code+"'"+' | nc 127.0.0.1 5858').read()
 
-            stream_content = {'name': 'stdout', 'text': wl_response}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
+        stream_content = {'name': 'stdout', 'text': wl_response}
+        self.send_response(self.iopub_socket, 'stream', stream_content)
 
         return {'status': 'ok',
                 'execution_count': self.execution_count,
@@ -38,18 +35,17 @@ class JWLS_2_kernel(Kernel):
                }
                
     
-    
-    
+    ##################################################################################
     def do_complete(self, code, cursor_pos):
         code = code[:cursor_pos]
         default = {'matches': [], 'cursor_start': 0,
-                   'cursor_end': cursor_pos, 'metadata': dict(),
+                   'cursor_end': cursor_pos,
                    'status': 'ok'}
-
+       
         if not code or code[-1] == ' ':
             return default
-
-        tokens = code.replace(';', ' ').replace('@', ' ').replace('/', ' '
+        
+        token =  code.replace(';', ' ').replace('@', ' ').replace('/', ' '
                     ).replace('?', ' ').replace(',', ' ').replace('.', ' '
                     ).replace('>', ' ').replace('<', ' ').replace(':', ' '
                     ).replace('[', ' ').replace(']', ' ').replace('(', ' '
@@ -57,19 +53,12 @@ class JWLS_2_kernel(Kernel):
                     ).replace('_', ' ').replace('+', ' ').replace('*', ' '
                     ).replace('#', ' ').replace('=', ' ').replace('"', ' '
                     ).replace('~', ' ').replace('&', ' ').replace('|', ' '
-                    ).replace('!', ' ').split()
-        
-        if not tokens:
-            return default
-
-        matches = WNames
-        token = tokens[-1]
+                    ).replace('!', ' ').split()[-1]
+                            
         start = cursor_pos - len(token)
 
-        if not matches:
-            return default
-        matches = [m for m in matches if m.startswith(token)]
+        matches = [m for m in WNames if m.startswith(token)]
 
         return {'matches': sorted(matches), 'cursor_start': start,
-                'cursor_end': cursor_pos, 'metadata': dict(),
+                'cursor_end': cursor_pos,
                 'status': 'ok'}
